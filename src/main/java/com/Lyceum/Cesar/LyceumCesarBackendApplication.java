0340b1fe0f1abad72d0e963d47d08db00923000d
TarcisioLuiz <tarcisioluizfilho@gmail.com>
2025-09-22 18:43:53 -0300

    first commit

---
+++ src/main/java/com/Lyceum/Cesar/LyceumCesarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.Lyceum.Cesar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyceumCesarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyceumCesarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Lyceum/Cesar/LyceumCesarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Lyceum.Cesar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyceumCesarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
